f6ce9addf34544710dbd949e7e29d87f6ef0ace6
SOLARSITES <iourion@gmail.com>
2021-05-29 14:20:29 +0300

    added: lesson02_advanced done

--- script.js
+++ script.js
@@ -1,22 +1,13 @@
-let money = 30000;
-let income = 'Фриланс';
-let addExpenses = 'Коммуналка, Транспорт, Интернет';
-let deposit = true;
-let mission = 1000000;
-let period = 6;
+'use strict';
 
-console.log(typeof money);
-console.log(typeof income);
-console.log(typeof deposit);
+let num = 266219;
 
-console.log(addExpenses.length);
+let multResult = num
+  .toString()
+  .split('')
+  .reduce((total, amount) => total * amount);
+console.log(multResult);
 
-console.log('Период равен ' + period + ' месяцев');
-console.log('Цель заработать ' + mission + ' рублей/долларов/гривен/юани');
+let degResult = multResult ** 3;
 
-let lowerCase = addExpenses.toLocaleLowerCase();
-let showArray = lowerCase.split(', ');
-console.log(showArray);
-
-let budgetDay = money / 30;
-console.log(budgetDay);
+console.log(String(degResult).slice(0, 2));
