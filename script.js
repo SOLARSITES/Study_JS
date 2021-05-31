c77a84193e17fb7b51a362960ad2e0bba5a72fdf
SOLARSITES <iourion@gmail.com>
2021-05-31 21:49:10 +0300

    added: paragraph 1.a solution

---
+++ script.js
@@ -0,0 +1,9 @@
+'use strict';
+
+let lang = 'ru';
+let langOutput;
+if (lang === 'ru') {
+  langOutput = 'Пн, Вт, Ср, Чт, Пт, Сб, Вс';
+} else {
+  langOutput = 'Mon, Tue, Wed, Thu, Fri, Sat, Sun';
+}
