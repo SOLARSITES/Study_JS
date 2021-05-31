b2ea4814e237089d5cb1a1721b8725cd906c21c2
SOLARSITES <iourion@gmail.com>
2021-05-31 18:55:45 +0300

    fix: fixed code style in script.js

--- script.js
+++ script.js
@@ -7,13 +7,13 @@ const multResult = num
   .reduce((total, amount) => total * amount);
 const degResult = multResult ** 3;
 
-console.log(multResult);
-console.log(degResult.toString().substring(0, 2));
-
 // Вариант с использованием цикала для получения произведения цифр числа
 const strNum = num.toString();
 let mult = 1;
 for (let i = 0; i < strNum.length; i++) {
   mult *= strNum[i];
 }
+
+console.log(multResult);
+console.log(degResult.toString().substring(0, 2));
 console.log(mult);
