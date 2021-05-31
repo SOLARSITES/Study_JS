3daa60ad0d1ed2c007485816e8c282c64a94b703
SOLARSITES <iourion@gmail.com>
2021-05-31 18:05:39 +0300

    added: lesson02 draft

--- script.js
+++ script.js
@@ -1,22 +1,38 @@
-let money = 30000;
-let income = 'Фриланс';
-let addExpenses = 'Коммуналка, Транспорт, Интернет';
-let deposit = true;
-let mission = 1000000;
+'use strict';
+
+let money = +prompt('Ваш месячный доход?');
+let addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую');
+let deposit = confirm('Есть ли у вас депозит в банке?');
+let expenses1 = prompt('Введите обязательную статью расходов?');
+let expenses2 = prompt('Введите обязательную статью расходов?');
+let amount1 = +prompt('Во сколько это обойдется?');
+let amount2 = +prompt('Во сколько это обойдется?');
+let mission = 200000;
+let budgetMonth = money - amount1 - amount2;
+let budgetDay = budgetMonth / 30;
 let period = 6;
+let income = 'Фриланс';
+let lowerCase = addExpenses.toLocaleLowerCase();
+let showArray = lowerCase.split(', ');
+
+let levelBudgetDay = if (budgetDay >= 1200) {
+  console.log('У Вас высокий уровень дохода');
+} else if (budgetDay >= 600 && budgetDay <= 1200) {
+  console.log('У Вас средний уровень дохода');
+} else if (budgetDay >= 0 && budgetDay <= 600) {
+  console.log('К сожалению, у Вас уровень дохода ниже среднего');
+} else {
+  console.log('Что то пошло не так');
+}
 
 console.log(typeof money);
 console.log(typeof income);
 console.log(typeof deposit);
-
 console.log(addExpenses.length);
-
 console.log('Период равен ' + period + ' месяцев');
-console.log('Цель заработать ' + mission + ' рублей/долларов/гривен/юани');
-
-let lowerCase = addExpenses.toLocaleLowerCase();
-let showArray = lowerCase.split(', ');
+console.log('Цель заработать ' + mission + ' рублей');
+console.log(addExpenses);
 console.log(showArray);
-
-let budgetDay = money / 30;
-console.log(budgetDay);
+console.log('Бюджет на месяц: ' + budgetMonth);
+console.log('Цель будет достигнута за ' + Math.ceil(mission / budgetMonth) + ' месяцев(-а)');
+console.log('Бюджет на день: ' + Math.floor(budgetDay));
