16abaa88656d54ac2e89a933a4b9f63ba206a42a
SOLARSITES <iourion@gmail.com>
2021-05-31 23:00:53 +0300

    added: paragraph 1.b and 1.c solutions

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
 'use strict';
 
 let lang = 'ru';
-let langOutput;
+let langOutput1;
+let langOutput2;
+let langOutput3 = [['Пн, Вт, Ср, Чт, Пт, Сб, Вс'], ['Mon, Tue, Wed, Thu, Fri, Sat, Sun']];
+
 if (lang === 'ru') {
-  langOutput = 'Пн, Вт, Ср, Чт, Пт, Сб, Вс';
+  langOutput1 = 'Пн, Вт, Ср, Чт, Пт, Сб, Вс';
 } else {
-  langOutput = 'Mon, Tue, Wed, Thu, Fri, Sat, Sun';
+  langOutput1 = 'Mon, Tue, Wed, Thu, Fri, Sat, Sun';
 }
+switch (lang) {
+  case 'ru':
+    langOutput2 = 'Пн, Вт, Ср, Чт, Пт, Сб, Вс';
+    break;
+  default:
+    langOutput2 = 'Mon, Tue, Wed, Thu, Fri, Sat, Sun';
+}
+
+console.log(langOutput1);
+console.log(langOutput2);
+console.log(langOutput3[0].toString());
