fe687235dc307192025ea71e149739abc4a21589
SOLARSITES <iourion@gmail.com>
2021-06-03 14:42:52 +0300

    added: lesson05_advanced draft

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 'use strict';
 
 let arr = ['46104', '15546', '2291219', '7033', '402847', '613705', '21237460'];
+
+for (let i = 0; i < arr.length; i++) {
+  if (arr[i].startsWith('2') || arr[i].startsWith('4')) {
+    console.log(arr[i]);
+  }
+}
