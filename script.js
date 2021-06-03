a047fb47a63d844873aad15283d62a89bfd5d5ed
SOLARSITES <iourion@gmail.com>
2021-06-03 16:45:35 +0300

    added: lesson05_advanced done

--- script.js
+++ script.js
@@ -7,3 +7,24 @@ for (let i = 0; i < arr.length; i++) {
     console.log(arr[i]);
   }
 }
+
+let getPrimes = function (prime) {
+  let arrPrimes = [];
+  if (prime !== 1) {
+    arrPrimes.push(1);
+    for (let j = 2; j * j <= prime; j++) {
+      if (prime % j === 0) {
+        arrPrimes.push(j);
+      }
+    }
+  }
+  arrPrimes.push(prime);
+  return arrPrimes;
+};
+
+for (let i = 2; i <= 100; i++) {
+  let n = getPrimes(i);
+  if (n.length <= 2) {
+    console.log(`${i}: Простое число. Делители этого числа: ${n.join(' и ')}`);
+  }
+}
