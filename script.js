7f40ddcf286981b3d544d7e4d69e5ab54fa5a56f
SOLARSITES <iourion@gmail.com>
2021-06-08 15:34:21 +0300

    added: lesson10 draft4

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 'use strict';
 
+const divBookAll = document.querySelectorAll('.book');
+
 const asideBooks = document.querySelector('.books');
+const asideBooksAll = document.querySelectorAll('.books');
 const divBook0 = document.querySelectorAll('.book')[0];
 const divBook1 = document.querySelectorAll('.book')[1];
 const divBook2 = document.querySelectorAll('.book')[2];
@@ -10,9 +13,16 @@ const divBook5 = document.querySelectorAll('.book')[5];
 
 asideBooks.insertBefore(divBook1, divBook0);
 asideBooks.insertBefore(divBook4, divBook2);
-// asideBooks.insertBefore(divBook4, divBook3);
+asideBooks.insertBefore(divBook3, divBook2);
+asideBooks.insertBefore(divBook5, divBook2);
+
+document.querySelector('body').style.backgroundImage = 'url(image/you-dont-know-js.jpg)';
+
+// divBook4.document.querySelector('h2').innerText = 'Книга 3. this и Прототипы Объектов';
+
+divBook4.childNodes[1].childNodes[1].innerText = 'Книга 3. this и Прототипы Объектов';
 
 document.querySelector('.adv').remove();
 
-console.log(asideBooks);
-console.log(divBook1);
+console.log(asideBooksAll);
+console.log(divBookAll);
