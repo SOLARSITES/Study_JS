a6f0182ff9d1c652bab979a794695ca868e515ed
SOLARSITES <iourion@gmail.com>
2021-06-10 21:41:04 +0300

    added: lesson13_advanced done

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const getRandomColor = function () {
+  let rawColorString = '0123456789abcdef',
+    getColor = '#';
+  for (let i = 0; i < 6; i++) {
+    getColor += rawColorString[Math.floor(Math.random() * 16)];
+  }
+  return getColor;
+};
+
+const setBodyColor = function () {
+  const newBodyColor = getRandomColor();
+  document.body.style.backgroundColor = newBodyColor;
+  document.querySelector('h1').textContent = newBodyColor;
+};
+
+document.querySelector('.button').addEventListener('click', setBodyColor);
