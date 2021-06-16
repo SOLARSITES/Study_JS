3c2906cf2ec341c66e5f8f26497699693db9c2ed
SOLARSITES <iourion@gmail.com>
2021-06-16 17:31:50 +0300

    added: lesson17 draft3

--- script/script.js
+++ script/script.js
@@ -1,11 +1,11 @@
 window.addEventListener('DOMContentLoaded', () => {
   // Timer
-  function countTimer(deadline) {
+  const countTimer = (deadline) => {
     let timerHours = document.querySelector('#timer-hours'),
       timerMinutes = document.querySelector('#timer-minutes'),
       timerSeconds = document.querySelector('#timer-seconds');
 
-    function getTimeRemaining() {
+    const getTimeRemaining = () => {
       let dateStop = new Date(deadline).getTime(),
         dateNow = new Date().getTime(),
         timeRemaining = (dateStop - dateNow) / 1000,
@@ -14,23 +14,33 @@ window.addEventListener('DOMContentLoaded', () => {
         hours = Math.floor(timeRemaining / 60 / 60) % 24;
       // day = Math.floor(timeRemaining / 60 / 60 / 24);
       return {
-        hours: hours,
-        minutes: minutes,
-        seconds: seconds,
+        timeRemaining,
+        hours,
+        minutes,
+        seconds,
       };
-    }
+    };
 
-    // timerHours.textContent = hours;
-    // timerMinutes.textContent = minutes;
-    // timerSeconds.textContent = seconds;
+    const updateClock = () => {
+      let timer = getTimeRemaining();
 
-    console.log(getTimeRemaining());
+      timerHours.textContent = timer.hours;
+      timerMinutes.textContent = timer.minutes;
+      timerSeconds.textContent = timer.seconds;
+
+      if (timer.timeRemaining > 0) {
+        setTimeout(updateClock, 1000);
+      }
+    };
+
+    updateClock();
+
     // console.log(seconds);
     // console.log(minutes);
     // console.log(hours);
     // console.log(day);
-  }
+  };
 
-  // countTimer();
+  countTimer('01 july 2021');
   // setInterval(countTimer, 1000, '01 july 2021');
 });
