3d765eafd5318e93351a70970c0dd086982055f7
SOLARSITES <iourion@gmail.com>
2021-06-17 15:45:55 +0300

    added: lesson18 draft2

--- js/script.js
+++ js/script.js
@@ -1,47 +1,3 @@
-// const countTimer = (deadline) => {
-//   let timerHours = document.querySelector('#timer-hours'),
-//     timerMinutes = document.querySelector('#timer-minutes'),
-//     timerSeconds = document.querySelector('#timer-seconds');
-//
-//   const getTimeRemaining = () => {
-//     let dateStop = new Date(deadline).getTime(),
-//       dateNow = new Date().getTime(),
-//       timeRemaining = (dateStop - dateNow) / 1000,
-//       seconds = Math.floor(timeRemaining % 60),
-//       minutes = Math.floor((timeRemaining / 60) % 60),
-//       hours = Math.floor(timeRemaining / 60 / 60) % 24;
-//     // day = Math.floor(timeRemaining / 60 / 60 / 24);
-//     return {
-//       timeRemaining,
-//       hours,
-//       minutes,
-//       seconds,
-//     };
-//   };
-//
-//   const updateClock = () => {
-//     let timer = getTimeRemaining();
-//
-//     timerHours.textContent = timer.hours;
-//     timerMinutes.textContent = timer.minutes;
-//     timerSeconds.textContent = timer.seconds;
-//
-//     if (timer.timeRemaining > 0) {
-//       setTimeout(updateClock, 1000);
-//     }
-//   };
-//
-//   updateClock();
-//
-//   // console.log(seconds);
-//   // console.log(minutes);
-//   // console.log(hours);
-//   // console.log(day);
-// };
-//
-// countTimer('01 july 2021');
-// // setInterval(countTimer, 1000, '01 july 2021');
-
 // Timer
 const countTimer = (deadline) => {
   const timerHours = document.querySelector('#timer-hours'),
@@ -101,3 +57,46 @@ const countTimer = (deadline) => {
 };
 
 countTimer('17 Jun 2021');
+
+// Menu
+const toggleMenu = () => {
+  const btnMenu = document.querySelector('.menu'),
+    menu = document.querySelector('menu'),
+    closeBtn = document.querySelector('.close-btn'),
+    menuItems = menu.querySelectorAll('ul>li');
+
+  const handlerMenu = () => {
+    menu.classList.toggle('active-menu');
+
+    // if (!menu.style.transform || menu.style.transform === `translate(-100%)`) {
+    //   menu.style.transform = `translate(0)`;
+    // } else {
+    //   menu.style.transform = `translate(-100%)`;
+    // }
+  };
+
+  btnMenu.addEventListener('click', handlerMenu);
+  closeBtn.addEventListener('click', handlerMenu);
+  menuItems.forEach((elem) => elem.addEventListener('click', handlerMenu));
+};
+
+toggleMenu();
+
+// Popup
+const togglePopUp = () => {
+  const popup = document.querySelector('.popup'),
+    popupBtn = document.querySelectorAll('.popup-btn'),
+    popupClose = document.querySelector('.popup-close');
+
+  popupBtn.forEach((elem) => {
+    elem.addEventListener('click', () => {
+      popup.style.display = 'block';
+    });
+  });
+
+  popupClose.addEventListener('click', () => {
+    popup.style.display = 'none';
+  });
+};
+
+togglePopUp();
