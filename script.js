192b07e817eba2418e49601c5eeb20e31d0f8098
SOLARSITES <iourion@gmail.com>
2021-06-29 20:01:26 +0300

    added: lesson28_advanced done

--- script.js
+++ script.js
@@ -1,25 +1,50 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const select = document.getElementById('cars'),
-    output = document.getElementById('output');
+'use strict';
 
-  select.addEventListener('change', () => {
-    const request = new XMLHttpRequest();
-    request.open('GET', './cars.json');
-    request.setRequestHeader('Content-type', 'application/json');
-    request.send();
-    request.addEventListener('readystatechange', () => {
-      if (request.readyState === 4 && request.status === 200) {
-        const data = JSON.parse(request.responseText);
-        data.cars.forEach((item) => {
-          if (item.brand === select.value) {
-            const { brand, model, price } = item;
-            output.innerHTML = `Тачка ${brand} ${model} <br>
-                        Цена: ${price}$`;
-          }
-        });
-      } else {
-        output.innerHTML = 'Произошла ошибка';
+document.addEventListener('DOMContentLoaded', () => {
+  const select = document.getElementById('cars');
+  const output = document.getElementById('output');
+  const dbCars = './cars.json';
+
+  const getData = (urlCars) => {
+    return new Promise((resolve, reject) => {
+      const request = new XMLHttpRequest();
+
+      request.open('GET', urlCars);
+      request.setRequestHeader('Content-type', 'application/json');
+      request.send();
+
+      request.addEventListener('readystatechange', () => {
+        if (request.readyState !== 4) {
+          return;
+        }
+
+        if (request.readyState === 4 && request.status === 200) {
+          resolve(JSON.parse(request.responseText));
+        } else {
+          reject(request.statusText);
+        }
+      });
+    });
+  };
+
+  const showData = (data) => {
+    data.cars.forEach((item) => {
+      if (item.brand === select.value) {
+        const { brand, model, price } = item;
+
+        output.innerHTML = `Тачка ${brand} ${model} <br>
+						Цена: ${price}$`;
       }
     });
+  };
+
+  select.addEventListener('change', () => {
+    getData(dbCars)
+      .then((data) => {
+        showData(data);
+      })
+      .catch((error) => {
+        output.innerHTML = `Произошла ошибка: ${error}`;
+      });
   });
 });
