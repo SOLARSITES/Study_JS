5ffa62fc1b57f2f9660462ae07182369f98ac006
SOLARSITES <iourion@gmail.com>
2021-07-12 08:27:04 +0300

    added: add module structure

--- src/index.js
+++ src/index.js
@@ -1,33 +1,24 @@
 // Modules Import
-import countTimer from './modules/countTimer';
-import toggleMenu from './modules/toggleMenu';
-import togglePopUp from './modules/togglePopUp';
-import tabs from './modules/tabs';
-import addDot from './modules/addDot';
-import setCommandImg from './modules/setCommandImg';
-import calc from './modules/calc';
-import slider from './modules/slider';
-import sendForm from './modules/sendForm';
+// import toggleMenu from './modules/toggleMenu';
+// import togglePopUp from './modules/togglePopUp';
+// import tabs from './modules/tabs';
+// import addDot from './modules/addDot';
+// import slider from './modules/slider';
+// import sendForm from './modules/sendForm';
 import SliderCarousel from './modules/sliderCarousel';
 
-// Timer
-countTimer('17 Jun 2021');
 // Menu
-toggleMenu();
+// toggleMenu();
 // Popup
-togglePopUp();
+// togglePopUp();
 // Tabs
-tabs();
+// tabs();
 // Add .dot
-addDot();
-// Change Command Photos
-setCommandImg();
-// Calculator
-calc(100);
+// addDot();
 // Slider
-slider();
+// slider();
 // Send-AJAX-Form
-sendForm();
+// sendForm();
 // Slider Carousel
 const sliderCarousel = new SliderCarousel({
   main: '.companies-wrapper',
