45c1fb2fa98c928e22cb738ea929e762bc8ae701
SOLARSITES <iourion@gmail.com>
2021-07-26 16:22:28 +0300

    added: modals animations by animate module

--- src/modules/modal.js
+++ src/modules/modal.js
@@ -1,3 +1,4 @@
+import animate from './animate';
 import blockScroll from '../modules/blockScroll';
 
 const { disableScroll, enableScroll } = blockScroll;
@@ -8,14 +9,33 @@ const modal = () => {
 
   document.addEventListener('click', (e) => {
     if (e.target.matches('.callback-btn') || e.target.matches('.button-services')) {
+      modalCallback.style.transform = 'translate(-50%, -195%)';
+      modalOverlay.style.opacity = '0';
+
       modalCallback.style.display = 'block';
       modalOverlay.style.display = 'block';
+
+      animate({
+        duration: 450,
+        timing(timeFraction) {
+          return timeFraction;
+        },
+        draw(progress) {
+          modalCallback.style.transform = `translate(-50%, ${(1 - progress) * -195 - 55}%)`;
+          modalOverlay.style.opacity = progress;
+        },
+      });
+
       disableScroll();
     }
 
     if (e.target.closest('.modal-close') || e.target.matches('.modal-overlay')) {
       modalCallback.style.display = 'none';
       modalOverlay.style.display = 'none';
+
+      modalCallback.style.transform = 'translate(-50%, -195%)';
+      modalOverlay.style.opacity = '0';
+
       enableScroll();
     }
   });
--- src/modules/modalOrder.js
+++ src/modules/modalOrder.js
@@ -1,3 +1,4 @@
+import animate from './animate';
 import blockScroll from '../modules/blockScroll';
 
 const { disableScroll, enableScroll } = blockScroll;
@@ -9,14 +10,33 @@ const modalOrder = () => {
   const applicationInput = document.getElementById('applicationInput');
 
   const openModal = () => {
+    modalApplication.style.transform = 'translate(-50%, -195%)';
+    modalOverlay.style.opacity = '0';
+
     modalApplication.style.display = 'block';
     modalOverlay.style.display = 'block';
+
+    animate({
+      duration: 450,
+      timing(timeFraction) {
+        return timeFraction;
+      },
+      draw(progress) {
+        modalApplication.style.transform = `translate(-50%, ${(1 - progress) * -195 - 66}%)`;
+        modalOverlay.style.opacity = progress;
+      },
+    });
+
     disableScroll();
   };
 
   const closeModal = () => {
     modalApplication.style.display = 'none';
     modalOverlay.style.display = 'none';
+
+    modalApplication.style.transform = 'translate(-50%, -195%)';
+    modalOverlay.style.opacity = '0';
+
     enableScroll();
   };
 
